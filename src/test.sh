4035cc0873726fa08cd8178f9055ed9d5f1e66cc
jawher <yjawher213@gmail.com>
2025-10-15 17:05:01 +0100

    NodeJs app V2

--- src/test.sh
+++ src/test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-output=$(node -e "const greet = require('./src/app').greet; console.log(greet('Test'))")
+output=$(node -e "const greet = require('./app').greet; console.log(greet('Test'))")
 if [ "$output" = "Hello, Test!" ]; then
   echo "Test passed!"
   exit 0
